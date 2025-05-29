eece23ed2e84d8c769d2c4fbbd1db28f4f579fe4
Viraj Kavinda Meegahapola <91599142+virajkavinda95@users.noreply.github.com>
2025-05-29 11:22:10 +0400

    Update route.js

--- src/app/api/admin/countries/route.js
+++ src/app/api/admin/countries/route.js
@@ -1,50 +1,50 @@
-import { getServerSession } from "next-auth";
-import { NextResponse } from "next/server";
-import { authOptions } from "../../auth/[...nextauth]/route";
-// import dbConnect from "../../../lib/db";
-import Country from "../../../models/Country";
+// import { getServerSession } from "next-auth";
+// import { NextResponse } from "next/server";
+// import { authOptions } from "../../auth/[...nextauth]/route";
+// // import dbConnect from "../../../lib/db";
+// import Country from "../../../models/Country";
 
-export async function GET() {
-  const session = await getServerSession(authOptions);
+// export async function GET() {
+//   const session = await getServerSession(authOptions);
 
-  if (!session || session.user.role !== "admin") {
-    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-  }
+//   if (!session || session.user.role !== "admin") {
+//     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+//   }
 
-  // await dbConnect();
+//   // await dbConnect();
 
-  try {
-    const countries = await Country.find({}).sort({ name: 1 });
+//   try {
+//     const countries = await Country.find({}).sort({ name: 1 });
 
-    return NextResponse.json(countries);
-  } catch (e) {
-    return NextResponse.json(
-      { error: "Failed to fetch countries" },
-      { status: 500 }
-    );
-  }
-}
+//     return NextResponse.json(countries);
+//   } catch (e) {
+//     return NextResponse.json(
+//       { error: "Failed to fetch countries" },
+//       { status: 500 }
+//     );
+//   }
+// }
 
-// Post request
-export async function POST(request) {
-  const session = await getServerSession(authOptions);
+// // Post request
+// export async function POST(request) {
+//   const session = await getServerSession(authOptions);
 
-  if (!session || session.user.role !== "admin") {
-    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-  }
+//   if (!session || session.user.role !== "admin") {
+//     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+//   }
 
-  // await dbConnect();
+//   // await dbConnect();
 
-  try {
-    const body = await request.json();
+//   try {
+//     const body = await request.json();
 
-    const country = await Country.create(body);
+//     const country = await Country.create(body);
 
-    return NextResponse.json(country);
-  } catch (e) {
-    return NextResponse.json(
-      { error: "Failed to fetch countries" },
-      { status: 500 }
-    );
-  }
-}
+//     return NextResponse.json(country);
+//   } catch (e) {
+//     return NextResponse.json(
+//       { error: "Failed to fetch countries" },
+//       { status: 500 }
+//     );
+//   }
+// }
